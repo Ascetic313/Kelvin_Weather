0a62c99fbec43546ba11eacfe27bdeadfe6d6cc1
Ascetic313 <iddrisualmuqadas74@gmail.com>
2022-01-01 16:55:00 +0000

    This is a project on Kelvin Weather

---
+++ Kelvin_Weather.js
@@ -0,0 +1,14 @@
+//Creating a variable named: Kelvin//
+const Kelvin = 293;
+
+// Converting Kelvin to Celsius//
+var celsius = 0 - 273;
+
+// Inputing the Fahrenheit equation://
+var fahrenheit = celsius * (9/5) + 32;
+
+//Rounding down the Temperature in Fahrenheit//
+fahrenheit = Math.floor(fahrenheit);
+
+// Logging the Temperature in Fahrenheit//
+console.log(`The temperature is ${fahrenheit}  Fahrenheit`)
